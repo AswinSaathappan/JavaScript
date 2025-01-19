eb12918b9cf0f21287ddc375f712b194f848fa05
Aswin Saathappan A <aswinsaathappan14@gmail.com>
2025-01-19 10:31:02 +0530

    Add files via upload

---
+++ Introduction/script.js
@@ -0,0 +1,9 @@
+console.log("Happy New Year");
+alert("Some is thretening you");
+let score = 100078;
+console.log("Your bill amount is Rs."+score);
+let name = prompt("What is your name?");
+console.log("hi"+name);
+let a = prompt("Mr.അശ്വിൻ സാത്തപ്പൻ Enter a number:");
+console.log(a+10);//Whaterver we give in prompt it is of type string 
+console.log(Number(a)+10);
