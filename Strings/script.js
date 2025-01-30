7d072a95ae414b5b17e420d040581cb5aeb7c56f
AswinSaathappan <aswinsaathappan14@gmail.com>
2025-01-30 20:32:54 +0530

    added Strings

---
+++ Strings/script.js
@@ -0,0 +1,20 @@
+let str1 = "Aswin";
+let str2 = "Saaathppan";
+console.log("str1:"+str1+" "+"str2:"+str2);
+console.log(str1+str2);
+console.log(str1.concat(str2))
+console.log(str2.slice(1,4))
+console.log(str2.slice(-4))
+console.log(str1.toLocaleUpperCase())
+console.log(str1.length)
+console.log(str2.length)
+console.log(str1.charAt(2))
+console.log(str1[2])
+console.log(str1.indexOf('w'))
+console.log(str1.lastIndexOf('w'))
+console.log(str2.search("ppan"))
+console.log(str2.includes("ppan"))
+console.log(str1.startsWith("a"))
+console.log(str2.endsWith('n'))
+console.log(str1.padEnd(9,'a'))
+console.log(str1.padStart(9,'a'))
