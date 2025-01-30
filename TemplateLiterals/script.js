e8127e7c610ec851a3c1f5a6b1704540bb03d54e
AswinSaathappan <aswinsaathappan14@gmail.com>
2025-01-30 20:33:53 +0530

    added TemplateLiterals

---
+++ TemplateLiterals/script.js
@@ -0,0 +1,14 @@
+let fname ="Aswin"
+let lname = "Saathappan"
+let city = "Madurai"
+
+console.log(fname + " " + lname  + "  lives in " + city)
+console.log(`${fname} ${lname} lives in ${city}`)
+
+let msg = `Happy
+Birthday`
+
+console.log(msg)
+
+msg = `"Cat's name is "toto"."`
+console.log(msg)
